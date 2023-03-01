e6180b851a539b8af9b1d84e755fe7b73cc15124
Kruben Naidoo <naidook@cput.ac.za>
2023-03-01 09:17:10 +0200

    added method to return the sum of two String values

--- src/main/java/za/ac/cput/Calc.java
+++ src/main/java/za/ac/cput/Calc.java
@@ -20,4 +20,14 @@ public class Calc {
     public static double add(double a, double b) {
         return (a + b);
     }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return String
+     */
+    public static String add(String a, String b) {
+        return (a + b);
+    }
 }
